41fece6f4520db59e5b0dc23f53e27bcccb82a57
IdreesDweik <idrees.dweik@gmail.com>
2023-07-09 14:04:30 +0300

    js

---
+++ main.js
@@ -0,0 +1,10 @@
+const myHonda = {
+    color: "red",
+    wheels: 4,
+    engine: { cylinders: 4, size: 2.2 },
+  };
+
+  let theName ="hasan";
+  let theName1 ='hasan';
+  let theName2 ='"hasan"';
+  let theName3 ="'hasan'";
