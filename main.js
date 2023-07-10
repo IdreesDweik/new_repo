3fc8c9f586dd038592636704caf66807052b1236
IdreesDweik <idrees.dweik@gmail.com>
2023-07-10 12:29:57 +0300

    js

--- main.js
+++ main.js
@@ -1,10 +1,107 @@
-const myHonda = {
-    color: "red",
-    wheels: 4,
-    engine: { cylinders: 4, size: 2.2 },
-  };
+// const myHonda = {
+//     color: "red",
+//     wheels: 4,
+//     engine: { cylinders: 4, size: 2.2 },
+//     pass5miles : function () {
+//       console.log('done')
+//     }
+//   };
 
-  let theName ="hasan";
-  let theName1 ='hasan';
-  let theName2 ='"hasan"';
-  let theName3 ="'hasan'";
+//   let theName  = "hasan";
+//   let theName1 = 'hasan';
+//   let theName2 = '"hasan"';
+//   let theName3 = "'hasan'";
+ 
+  // var let const
+  // scope types (global/block)
+  // block scope (function/block)
+
+//  var x   = 1;
+//  let y   = 2;
+//  const z = 3;
+
+
+//  if(true){
+//   console.log(x,y,z) 
+//   }
+  
+  
+
+  // function test(){
+  //   console.log(x,y,z) 
+  // }
+  // test();
+
+
+  /*
+  Logical Operators
+  - ! Not
+  - && And
+  - || Or
+*/
+
+// console.log(!(7 == "7"));
+
+// console.log(7 == "7" && 7 > 8 && 7 >= 7);
+
+// console.log(7 == "7" || 7 > 8 || 7 > 7);
+
+//switch
+
+// let Month = Number (prompt("Enter your Month of birth (examble: 1,2,3..."));
+
+// switch (Month) {
+//   case 1:
+//     Month = "January";
+//     break;
+//   case 2:
+//     Month = "February";
+//     break;
+//   case 3:
+//     Month = "March";
+//     break;
+//   case 4:
+//     Month = "April";
+//     break;
+//   case 5:
+//     Month = "May";
+//     break;
+//   case 6:
+//     Month = "June";
+//     break;
+//   case 7:
+//     Month = "July";
+//     break;
+//   case 8:
+//     Month = "August";
+//     break;
+//   case 9:
+//     Month = "September";
+//     break;
+//   case 10:
+//     Month = "October";
+//     break;
+//   case 11:
+//     Month = "November";
+//     break;
+//   case 12:
+//     Month = "December";
+//     break;
+//   default:
+//     Month = "Invalid month";
+//     break;
+// }
+
+// console.log("The output: " + Month);
+// document.write("<h2>Month: " + Month + "</h2>");
+
+
+
+// while
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
