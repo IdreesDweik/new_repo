c445c258916d57887fca08a38dfc33cf0be7085f
IdreesDweik <idrees.dweik@gmail.com>
2023-07-16 14:57:03 +0300

    JS

---
+++ main3.js
@@ -0,0 +1,11 @@
+const array1 = [1, 4, 9, 16, 36, 49];
+const map1 = array1.map(n => n * 2 / 4);
+const roots = array1.map((num) => Math.sqrt(num));
+
+console.log(array1);
+console.log(map1);
+console.log(roots);
+array1.forEach(Element => console.log(Element));
+
+
+
